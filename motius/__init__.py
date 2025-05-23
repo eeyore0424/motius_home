4a45497dd05fe3439b1c85a36d39ba8bd2ae1b75
eeyore0424 <eeyore0424@gmail.com>
2025-05-23 18:06:00 +0900

    홈페이지 연결

--- motius/__init__.py
+++ motius/__init__.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
     
     @app.route('/')
     def hello_motius():
-        return 'Hello, Motius!'
+        return render_template('index.html')
+    
+    @app.route('/terms')
+    def terms():
+        return render_template('terms.html')
+    
+    @app.route('/privacy')
+    def privacy():
+        return render_template('privacy.html')
+    
+    # @app.route('/static/<path:filename>')
+    # def static_file(filename):
+    #     return send_from_directory('static', filename)
     
     return app
